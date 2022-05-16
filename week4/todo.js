f08e8c5537f41cc31ec93636f5bddc52da6c7862
Krystle5183 <96323148+Krystle5183@users.noreply.github.com>
2022-05-15 22:34:57 -0500

    Add files via upload

---
+++ week4/todo.js
@@ -0,0 +1,32 @@
+axios.get("http://api.bryanuniversity.edu/krystle5183/list")
+    .then(response => {
+
+    for(let i = 0; i < response.data.length; i++) {
+
+        const newDiv = document.createElement("div")
+        document.body.appendChild(newDiv)
+
+        const newInputBox = document.createElement("input")
+        newInputBox.setAttribute("type","checkbox")
+        newDiv.appendChild(newInputBox)
+
+        const newLabel = document.createElement("label")
+        newLabel.textContent = response.data[i].title
+        newDiv.appendChild(newLabel)
+
+        const newP1 = document.createElement("p")
+        newP1.textContent = "Description: " + response.data[i].description
+        newDiv.appendChild(newP1)
+
+        const newP2 = document.createElement("p")
+        newP2.textContent = "Price: " + response.data[i].price
+        newDiv.appendChild(newP2)
+
+        if (response.data[i].completed === true) {
+            newLabel.style.textDecoration = 'line-through'
+        }
+          
+    }
+})   
+
+.catch()
